a4fbb8c590965493d59b8d94fe0d4aceed770167
dhanush251201 <20z214@psgtech.ac.in>
2022-09-06 23:24:41 +0530

    hello

--- socket one way/client.c
+++ socket one way/client.c
@@ -17,17 +17,8 @@ int main(int argc, char const* argv[])
 	}
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(PORT);
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
-		<= 0) {
-		printf(
-			"\nInvalid address/ Address not supported \n");
-		return -1;
-	}
-
-	if ((client_fd
-		= connect(sock, (struct sockaddr*)&serv_addr,
-				sizeof(serv_addr)))
-		< 0) {
+	if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr,sizeof(serv_addr)))< 0)
+	{
 		printf("\nConnection Failed \n");
 		return -1;
 	}
